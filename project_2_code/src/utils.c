51d93381dc7b6421b3856378220050e41a0a0c09
Lev632 <114541520+Lev632@users.noreply.github.com>
2024-03-19 17:25:20 -0500

    Add files via upload

--- project_2_code/src/utils.c
+++ project_2_code/src/utils.c
@@ -1,7 +1,4 @@
 #include "utils.h"
-#include <cstdio>
-#include <stdio.h>
-
 
 
 const char* get_status_message(int status) {
@@ -17,15 +14,6 @@ const char* get_status_message(int status) {
 
 char *get_exe_name(char *path) {
     return strrchr(path, '/') + 1;
-}
-
-
-int get_tag(char *executable_name) {
-    unsigned int seed = 0;
-    for (int i = 0; i < strlen(executable_name); i++) {
-        seed += (int)executable_name[i];
-    }
-    return seed;
 }
 
 
@@ -79,6 +67,7 @@ char **get_student_executables(char *solution_dir, int *num_executables) {
             }
         }
     }
+
     // Close the directory
     closedir(dir);
 
@@ -86,24 +75,29 @@ char **get_student_executables(char *solution_dir, int *num_executables) {
     return executables;
 }
 
+
 // TODO: Implement this function
 int get_batch_size() {
-    int batchsize = 0;
-    FILE *cpuinfo = fopen("/proc/cpuinfo", "r");
-    if(cpuinfo == NULL){
-        perror("/proc/cpuinfo failed to open");
-        exit(EXIT_FAILURE);
+    FILE* cpuinfo_file = fopen("/proc/cpuinfo", "r");
+    if (cpuinfo_file == NULL) {
+        fprintf(stderr, "Error: Could not open /proc/cpuinfo\n");
+        exit(1);
     }
-    char buff[255];
-    while(fgets(buff, sizeof(buff), cpuinfo)!= NULL){
-        if(strstr(buff, "cpu cores") != NULL){
-            sscanf(buff, "cpu cores : %d", &batchsize);
-            break;
+
+    char line[256];
+    int processor_count = 0;
+
+    while (fgets(line, sizeof(line), cpuinfo_file)) {
+        if (strstr(line, "processor") != NULL) {
+            processor_count++;
         }
     }
-    fclose(cpuinfo);
-    return batchsize;
+
+    fclose(cpuinfo_file);
+    return processor_count;
+
 }
+
 
 // TODO: Implement this function
 void create_input_files(char **argv_params, int num_parameters) {
@@ -124,9 +118,40 @@ void create_input_files(char **argv_params, int num_parameters) {
         fprintf(file, "%s" , argv_params[i]);
         fclose(file);
     }
+
 }
 
 
+// TODO: Implement this function
+void start_timer(int seconds, void (*timeout_handler)(int)) {
+    // signal(SIGALRM, timeout_handler);
+    // alarm(seconds);
+    struct itimerval timer;
+
+    // Set up the signal handler for SIGALRM
+    signal(SIGALRM, timeout_handler);
+
+    // Configure the timer to expire after 'seconds' seconds and then repeatedly every 'seconds' seconds
+    timer.it_value.tv_sec = seconds;
+    timer.it_value.tv_usec = 0;
+    timer.it_interval.tv_sec = seconds;
+    timer.it_interval.tv_usec = 0;
+
+    // Set the timer
+    if (setitimer(ITIMER_REAL, &timer, NULL) == -1) {
+        // Error handling if setitimer fails
+    }
+
+}
+
+
+// TODO: Implement this function
+void cancel_timer() {
+    alarm(0);
+}
+
+
+// TODO: Implement this function
 void remove_input_files(char **argv_params, int num_parameters) {
     if(argv_params == NULL || num_parameters <= 0){
         perror("argv_params is either NULL or num_paramerter is less than 1. try again");
@@ -141,12 +166,21 @@ void remove_input_files(char **argv_params, int num_parameters) {
             exit(EXIT_FAILURE);
         }
     }
+
 }
 
 
 // TODO: Implement this function
 void remove_output_files(autograder_results_t *results, int tested, int current_batch_size, char *param) {
-    
+    //check this -----------------
+    for (int i = tested - current_batch_size; i < tested; i++) {
+        char *exe_name = get_exe_name(results[i].exe_path);
+        char filename[255];
+        sprintf(filename, "output/%s.%s", exe_name, param);
+        if (remove(filename) != 0) {
+            perror("Error removing output file");
+        }
+    }
 }
 
 
---
+++ project_2_code/src/worker.c
@@ -0,0 +1,207 @@
+#include "utils.h"
+
+// Run the (executable, parameter) pairs in batches of 8 to avoid timeouts due to 
+// having too many child processes running at once
+#define PAIRS_BATCH_SIZE 8
+
+typedef struct {
+    char *executable_path;
+    int parameter;
+    int status;
+} pairs_t;
+
+// Store the pairs tested by this worker and the results
+pairs_t *pairs;
+
+// Information about the child processes and their results
+pid_t *pids;
+int *child_status;     // Contains status of child processes (-1 for done, 1 for still running)
+
+int curr_batch_size;   // At most PAIRS_BATCH_SIZE (executable, parameter) pairs will be run at once
+long worker_id;        // Used for sending/receiving messages from the message queue
+
+
+// TODO: Timeout handler for alarm signal - should be the same as the one in autograder.c
+void timeout_handler(int signum) {
+    if(child_status == NULL)
+    {
+        return;
+    }
+    for(int i = 0 ; i < curr_batch_size; i++)
+    {
+        if(child_status[i] == 1)
+        {
+            kill(pids[i], SIGKILL);
+        }
+    }
+}
+
+
+// Execute the student's executable using exec()
+void execute_solution(char *executable_path, int param, int batch_idx) {
+ 
+    pid_t pid = fork();
+
+    // Child process
+    if (pid == 0) {
+        char *executable_name = get_exe_name(executable_path);
+
+        // TODO: Redirect STDOUT to output/<executable>.<input> file
+        char buffer[255];
+        sprintf(buffer, "output/%s", executable_name);
+        FILE *output = fopen(buffer, "w");
+        if(output == -1){
+            perror("opening file failed");
+            exit(EXIT_FAILURE);
+        }
+        if(dup2(output, 1) == -1){
+            perror("dup2 failed");
+            exit(EXIT_FAILURE);
+        }
+        fclose(output);
+
+        // TODO: Input to child program can be handled as in the EXEC case (see template.c)
+        pids[batch_idx] = atoi(argv[1]);
+        
+        perror("Failed to execute program in worker");
+        exit(1);
+    }
+    // Parent process
+    else if (pid > 0) {
+        pids[batch_idx] = pid;
+    }
+    // Fork failed
+    else {
+        perror("Failed to fork");
+        exit(1);
+    }
+}
+
+
+// Wait for the batch to finish and check results
+void monitor_and_evaluate_solutions(int finished) {
+    // Keep track of finished processes for alarm handler
+    child_status = malloc(curr_batch_size * sizeof(int));
+    for (int j = 0; j < curr_batch_size; j++) {
+        child_status[j] = 1;
+    }
+
+    // MAIN EVALUATION LOOP: Wait until each process has finished or timed out
+    for (int j = 0; j < curr_batch_size; j++) {
+        char *current_exe_path = pairs[finished + j].executable_path;
+        int current_param = pairs[finished + j].parameter;
+
+        int status;
+        pid_t pid = waitpid(pids[j], &status, 0);
+
+        // TODO: What if waitpid is interrupted by a signal?
+
+
+        int exit_status = WEXITSTATUS(status);
+        int exited = WIFEXITED(status);
+        int signaled = WIFSIGNALED(status);
+
+        // TODO: Check if the process finished normally, segfaulted, or timed out and update the 
+        //       pairs array with the results. Use the macros defined in the enum in utils.h for 
+        //       the status field of the pairs_t struct (e.g. CORRECT, INCORRECT, SEGFAULT, etc.)
+        //       This should be the same as the evaluation in autograder.c, just updating `pairs` 
+        //       instead of `results`.
+
+
+        // Mark the process as finished
+        child_status[j] = -1;
+    }
+
+    free(child_status);
+}
+
+
+// Send results for the current batch back to the autograder
+void send_results(int msqid, long mtype, int finished) {
+    // Format of message should be ("%s %d %d", executable_path, parameter, status)
+
+}
+
+
+// Send DONE message to autograder to indicate that the worker has finished testing
+void send_done_msg(int msqid, long mtype) {
+    struct msg_buffer {
+    long type;
+    char text[100];
+    } message;
+
+    struct message msg_buf;
+    msg_buf.type = mtype;
+
+    if (msgsnd(msqid, &msg_buf, strlen(msg_buf.text) + 1, 0) == -1) {
+        perror("msgsnd");
+        exit(EXIT_FAILURE);
+    }
+
+
+}
+
+
+int main(int argc, char **argv) {
+    if (argc < 3) {
+        fprintf(stderr, "Usage: %s <msqid> <worker_id>\n", argv[0]);
+        return 1;
+    }
+
+    int msqid = atoi(argv[1]);
+    worker_id = atoi(argv[2]);
+
+    // TODO: Receive initial message from autograder specifying the number of (executable, parameter) 
+    // pairs that the worker will test (should just be an integer in the message body). (mtype = worker_id)
+
+    // TODO: Parse message and set up pairs_t array
+    int pairs_to_test;
+
+    // TODO: Receive (executable, parameter) pairs from autograder and store them in pairs_t array.
+    //       Messages will have the format ("%s %d", executable_path, parameter). (mtype = worker_id)
+
+    // TODO: Send ACK message to mq_autograder after all pairs received (mtype = BROADCAST_MTYPE)
+
+    // TODO: Wait for SYNACK from autograder to start testing (mtype = BROADCAST_MTYPE).
+    //       Be careful to account for the possibility of receiving ACK messages just sent.
+
+
+    // Run the pairs in batches of 8 and send results back to autograder
+    for (int i = 0; i < pairs_to_test; i+= PAIRS_BATCH_SIZE) {
+        int remaining = pairs_to_test - i;
+        curr_batch_size = remaining < PAIRS_BATCH_SIZE ? remaining : PAIRS_BATCH_SIZE;
+        pids = malloc(curr_batch_size * sizeof(pid_t));
+
+        for (int j = 0; j < curr_batch_size; j++) {
+            // TODO: Execute the student executable
+            execute_solution(pairs[i + j].executable_path, pairs[i + j].parameter, j);
+        }
+
+        // TODO: Setup timer to determine if child process is stuck
+            start_timer(TIMEOUT_SECS, timeout_handler);  // Implement this function (src/utils.c)
+
+        // TODO: Wait for the batch to finish and check results
+        monitor_and_evaluate_solutions(i);
+
+        // TODO: Cancel the timer if all child processes have finished
+        if (child_status == NULL) {
+            cancel_timer();
+        }
+
+        // TODO: Send batch results (intermediate results) back to autograder
+        send_results(msqid, worker_id, i);
+
+        free(pids);
+    }
+
+    // TODO: Send DONE message to autograder to indicate that the worker has finished testing
+    send_done_msg(msqid, worker_id);
+
+    // Free the pairs_t array
+    for (int i = 0; i < pairs_to_test; i++) {
+        free(pairs[i].executable_path);
+    }
+    free(pairs);
+
+    free(pids);
+}
