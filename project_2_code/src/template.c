125ee8addf46fe9bcab0f83942262e4d9bb53d6f
yeamelack <150837167+yeamelack@users.noreply.github.com>
2024-03-20 15:19:05 -0500

    Update template.c

--- project_2_code/src/template.c
+++ project_2_code/src/template.c
@@ -1,276 +1,87 @@
-#include "utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
 
 
-const char* get_status_message(int status) {
-    switch (status) {
-        case CORRECT: return "correct";
-        case INCORRECT: return "incorrect";
-        case SEGFAULT: return "crash";
-        case STUCK_OR_INFINITE: return "stuck/inf";
-        default: return "unknown";
-    }
+
+void infinite_loop() {
+    while(1){};    // Simulating a infinite loop
 }
 
+int main(int argc, char *argv[]) {
+    #ifndef REDIR
+        if (argc < 2) {
+            // Usage for  EXEC:  argv[0] <param>    # Input is just param
+            // Usage for  PIPE:  argv[0] <pipefd>   # Input is read end pipe fd
+            // Usage for REDIR:  argv[0]            # No param needed, read from stdin
+            printf("Usage: %s <parameter | pipefd>\n", argv[0]);
+            return 1;
+        }
+    #endif
 
-char *get_exe_name(char *path) {
-    return strrchr(path, '/') + 1;
-}
+    unsigned int seed = 0;
 
-
-char **get_student_executables(char *solution_dir, int *num_executables) {
-    DIR *dir;
-    struct dirent *entry;
-    struct stat st;
-
-    // Open the directory
-    dir = opendir(solution_dir);
-    if (!dir) {
-        perror("Failed to open directory");
-        exit(EXIT_FAILURE);
+    for (int i = 0; argv[0][i] != '\0'; i++) {
+        seed += (unsigned char)argv[0][i]; 
     }
 
-    // Count the number of executables
-    *num_executables = 0;
-    while ((entry = readdir(dir)) != NULL) {
-        // Ignore hidden files
-        char path[PATH_MAX];
-        sprintf(path, "%s/%s", solution_dir, entry->d_name);
+    unsigned int param = 0;
+
+    // TODO: Get input param from the different sources
+    #ifdef EXEC
+        param = atoi(argv[1]);
+
         
-        if (stat(path, &st) == 0) {
-            if (S_ISREG(st.st_mode) && entry->d_name[0] != '.')
-                (*num_executables)++;
-        } 
-        else {
-            perror("Failed to get file status");
-            exit(EXIT_FAILURE);
-        }
+
+    #elif REDIR
+        scanf("%d", &param);
+       
+        
+    #elif PIPE
+        int intarg = atoi(argv[1]);
+        read(intarg, &param, sizeof(param));
+        close(intarg);
+        
+    #endif
+
+    seed += param;
+    srandom(seed);
+  
+    int mode = random() % 5 + 1;
+    pid_t pid = getpid(); 
+
+    sleep(1); 
+
+    switch (mode) {
+        case 1:
+            // Using fprintf(stderr, ...) since STDOUT is redirected to a file
+            fprintf(stderr, "Program: %s, PID: %d, Mode: 1 - Exiting with status 0 (Correct answer)\n", argv[0], pid);
+            // TODO: Write the result (0) to the output file (output/<executable>.<input>)
+            //       Do not open the file. Think about what function you can use to output
+            //       information given what you redirected in the autograder.c file.
+            printf("%d", 0);
+            break;
+        case 2:
+            fprintf(stderr, "Program: %s, PID: %d, Mode: 2 - Exiting with status 1 (Incorrect answer)\n", argv[0], pid);
+            // TODO: Write the result (1) to the output file (same as case 1 above)
+            printf("%d", 1);
+            break;
+        case 3:
+            fprintf(stderr, "Program: %s, PID: %d, Mode: 3 - Triggering a segmentation fault\n", argv[0], pid);
+            raise(SIGSEGV);  // Trigger a segmentation fault
+            break;
+        case 4:
+            fprintf(stderr, "Program: %s, PID: %d, Mode: 4 - Entering an infinite loop\n", argv[0], pid);
+            infinite_loop();
+            break;
+        case 5:
+            fprintf(stderr, "Program: %s, PID: %d, Mode: 5 - Simulating being stuck/blocked\n", argv[0], pid);
+            pause();  // Simulate being stuck/blocked
+            break;
+        default:
+            break;
     }
 
-    // Allocate memory for the array of strings
-    char **executables = (char **) malloc(*num_executables * sizeof(char *));
-
-    // Reset the directory stream
-    rewinddir(dir);
-
-    // Read the file names
-    int i = 0;
-    while ((entry = readdir(dir)) != NULL) {
-        // Ignore hidden files
-        char path[PATH_MAX];
-        sprintf(path, "%s/%s", solution_dir, entry->d_name);
-
-        if (stat(path, &st) == 0) {
-            if (S_ISREG(st.st_mode) && entry->d_name[0] != '.') {
-                executables[i] = (char *) malloc((strlen(solution_dir) + strlen(entry->d_name) + 2) * sizeof(char));
-                sprintf(executables[i], "%s/%s", solution_dir, entry->d_name);
-                i++;
-            }
-        }
-    }
-
-    // Close the directory
-    closedir(dir);
-
-    // Return the array of strings (remember to free the memory later)
-    return executables;
-}
-
-
-// TODO: Implement this function
-int get_batch_size() {
-    FILE* cpuinfo_file = fopen("/proc/cpuinfo", "r");
-    if (cpuinfo_file == NULL) {
-        fprintf(stderr, "Error: Could not open /proc/cpuinfo\n");
-        exit(1);
-    }
-
-    char line[256];
-    int processor_count = 0;
-
-    while (fgets(line, sizeof(line), cpuinfo_file)) {
-        if (strstr(line, "processor") != NULL) {
-            processor_count++;
-        }
-    }
-
-    fclose(cpuinfo_file);
-    return processor_count;
-
-}
-
-
-// TODO: Implement this function
-void create_input_files(char **argv_params, int num_parameters) {
-    if(argv_params == NULL || num_parameters <= 0){
-        perror("argv_params is either NULL or num_paramerter is less than 1. try again");
-        exit(EXIT_FAILURE);
-    }
-    for(int i = 0; i<num_parameters; i++){
-        char filename[127];
-        sprintf(filename, "input/%s.in",argv_params[i]);
-        FILE *file = fopen(filename, "w");
-
-        if(file == NULL){
-            perror("failed opening student file");
-            exit(EXIT_FAILURE);
-        }
-
-        fprintf(file, "%s" , argv_params[i]);
-        fclose(file);
-    }
-
-}
-
-
-// TODO: Implement this function
-void start_timer(int seconds, void (*timeout_handler)(int)) {
-    struct sigaction sa;
-    memset(&sa, 0, sizeof(sa));
-    sa.sa_handler = timeout_handler;
-    sigemptyset(&sa.sa_mask);
-
-    if(sigaction(SIGALRM, &sa, NULL) == -1) {
-        perror("sigaction");
-        exit(EXIT_FAILURE);
-    }
-    alarm(TIMEOUT_SECS);
-}
-
-
-// TODO: Implement this function
-void cancel_timer() {
-    alarm(0);
-}
-
-
-// TODO: Implement this function
-void remove_input_files(char **argv_params, int num_parameters) {
-    if(argv_params == NULL || num_parameters <= 0){
-        perror("argv_params is either NULL or num_paramerter is less than 1. try again");
-        exit(EXIT_FAILURE);
-    }
-    for(int i=0; i<num_parameters; i++){
-        char filename[127];
-        sprintf(filename, "input/%s.in", argv_params[i]);
-
-        if(unlink(filename) != 0){
-            perror("cannot remove file");
-            exit(EXIT_FAILURE);
-        }
-    }
-
-}
-
-
-// TODO: Implement this function
-void remove_output_files(autograder_results_t *results, int tested, int current_batch_size, char *param) {
-    //check this -----------------
-    for (int i = tested - current_batch_size; i < tested; i++) {
-        char *exe_name = get_exe_name(results[i].exe_path);
-        char filename[255];
-        sprintf(filename, "output/%s.%s", exe_name, param);
-        if (remove(filename) != 0) {
-            perror("Error removing output file");
-        }
-    }
-}
-
-
-int get_longest_len_executable(autograder_results_t *results, int num_executables) {
-    int longest_len = 0;
-    for (int i = 0; i < num_executables; i++) {
-        char *exe_name = get_exe_name(results[i].exe_path);
-        int len = strlen(exe_name);
-        if (len > longest_len) {
-            longest_len = len;
-        }
-    }
-    return longest_len;
-}
- 
-
-void write_results_to_file(autograder_results_t *results, int num_executables, int total_params) {
-    FILE *file = fopen("results.txt", "w");
-    if (!file) {
-        perror("Failed to open file");
-        return;
-    }
-
-    // Find the longest executable name (for formatting purposes)
-    int longest_len = 0;
-    for (int i = 0; i < num_executables; i++) {
-        char *exe_name = get_exe_name(results[i].exe_path);
-        int len = strlen(exe_name);
-        if (len > longest_len) {
-            longest_len = len;
-        }
-    }
-
-    // Sort the results data structure by executable name (specifically number at the end)
-    for (int i = 0; i < num_executables; i++) {
-        for (int j = i + 1; j < num_executables; j++) {
-            char *exe_name_i = get_exe_name(results[i].exe_path);
-            int num_i = atoi(strrchr(exe_name_i, '_') + 1);
-            char *exe_name_j = get_exe_name(results[j].exe_path);
-            int num_j = atoi(strrchr(exe_name_j, '_') + 1);
-            if (num_i > num_j) {
-                autograder_results_t temp = results[i];
-                results[i] = results[j];
-                results[j] = temp;
-            }
-        }
-    }
-
-    // Write results to file
-    for (int i = 0; i < num_executables; i++) {
-        char *exe_name = get_exe_name(results[i].exe_path);
-
-        char format[20];
-        sprintf(format, "%%-%ds:", longest_len);
-        fprintf(file, format, exe_name); // Write the program path
-        for (int j = 0; j < total_params; j++) {
-            fprintf(file, "%5d (", results[i].params_tested[j]); // Write the pi value for the program
-            const char* message = get_status_message(results[i].status[j]);
-            fprintf(file, "%9s) ", message); // Write each status
-        }
-        fprintf(file, "\n");
-    }
-
-    fclose(file);
-}
-
-
-// TODO: Implement this function
-double get_score(char *results_file, char *executable_name) {
-    return 1.0;
-}
-
-
-void write_scores_to_file(autograder_results_t *results, int num_executables, char *results_file) {
-    for (int i = 0; i < num_executables; i++) {
-        double student_score = get_score(results_file, results[i].exe_path);
-        char *student_exe = get_exe_name(results[i].exe_path);
-
-        char score_file[] = "scores.txt";
-
-        FILE *score_fp;
-        if (i == 0)
-            score_fp = fopen(score_file, "w");
-        else
-            score_fp = fopen(score_file, "a");
-
-        if (!score_fp) {
-            perror("Failed to open score file");
-            exit(1);
-        }
-
-        int longest_len = get_longest_len_executable(results, num_executables);
-
-        char format[20];
-        sprintf(format, "%%-%ds: ", longest_len);
-        fprintf(score_fp, format, student_exe);
-        fprintf(score_fp, "%5.3f\n", student_score);
-
-        fclose(score_fp);
-    }
+    return 0;
 }
